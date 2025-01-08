9c6c8c7fc530379d3942e37a3134c3106be5ac38
Tafseer Ali <tafseeraliboy@gmail.com>
2025-01-08 14:49:01 +0530

    server.js

--- pdf_merger/server.js
+++ pdf_merger/server.js
@@ -24,7 +24,7 @@ app.post("/merge", upload.array("pdfs", 12), async (req, res) => {
     path.join(__dirname, req.files[1].path)
   );
 
-  res.redirect(`http://pdf-merger-p6fy.onrender.com/static//${d}.pdf`);
+  res.redirect(`http://pdf-merger-p6fy.onrender.com/static/${d}.pdf`);
 });
 
 app.listen(port, () => {
