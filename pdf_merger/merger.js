105f3d468d1085c3e1c552a2348037184095d7a8
Tafseer Ali <tafseeraliboy@gmail.com>
2025-01-08 14:18:11 +0530

    merger.js

--- pdf_merger/merger.js
+++ pdf_merger/merger.js
@@ -7,7 +7,7 @@ const mergepdfs = async (pdf1, pdf2) => {
   await merger.add(pdf1);
   await merger.add(pdf2);
   let d = new Date().getTime();
-  await merger.save(`/public/${d}.pdf`)
+  await merger.save(path.join(__dirname, `/public/${d}.pdf`))
     return d;
 };
 
